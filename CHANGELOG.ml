c335fe7b17b8257e67f65d6d315de60883a86179
Márcio Pessoa <marcio.pessoa@gmail.com>
2021-08-18 01:42:46 -0300

    Minor updates

--- CHANGELOG.ml
+++ CHANGELOG.ml
@@ -3,10 +3,6 @@
 ## Change log
 All notable changes to this project will be documented in this file.
 
-### [0.02] - 2020-03-11
-#### Added
-- Automatic conversion from classname to person name.
-
 ### [0.01] - 2020-03-08
 #### Added
 - First version.
